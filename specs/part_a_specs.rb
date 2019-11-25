b17d5e67a9dc27e19be78f5741b0d2a0ad83cb5b
Catherine Hall <catherine1992123@hotmail.com>
2019-11-25 15:38:24 +0000

    Set up file and spec file

---
+++ specs/part_a_specs.rb
@@ -0,0 +1,47 @@
+# Part A
+#
+# For this part we want you to make a class that represents a CodeClan student.
+#
+# Create a class called Student that takes in a name (String) and a cohort (string - e.g "E18", "G6", etc) when an new instance is created.
+# Create a couple of Getter methods, one that returns the name property and one that returns the cohort property of the student.
+# Add in Setter methods to update the students name and what cohort they are in.
+# Create a method that gets the student to talk (eg. Returns "I can talk!).
+# Create a method that takes in a students favourite programming language and returns it as part of a string (eg. student1.say_favourite_language("Ruby") -> "I love Ruby").
+
+
+require('minitest/autorun')
+require('minitest/reporters')
+require_relative('../part_a')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+class TestStudent < MiniTest::Test
+
+
+  def test_get_student_name
+    student = Student.new("Bob", "E36")
+    assert_equal("Bob", student.get_student_name())
+  end
+
+  def test_get_cohort
+    student = Student.new("Bob", "E36")
+    assert_equal("E36", student.get_cohort())
+  end
+
+  def test_set_student_name
+    student = Student.new("Bob", "E36")
+    student.get_student_name = "Fred"
+    assert_equal("Fred", student.set_student_name)
+  end
+
+  def test_set_cohort
+    student = Student.new("Bob", "E36")
+    student.get_cohort = "G1"
+    assert_equal("G1", student.set_cohort)
+  end
+
+
+
+
+
+
+end
